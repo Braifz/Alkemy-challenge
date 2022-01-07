6d42d08c0f8b1482d74fbc9fed0c29b8665a027c
Braifz <braian94lp@gmail.com>
2022-01-06 21:15:35 -0300

    fix typo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <Container className="mw-100 bg-secondary text-center shadow position-absolute bottom-0 shadow ">
             <p className="pt-3 font-monospace text-light">
-                made with @braifz from alkemy challenge
+                made by @braifz from alkemy challenge
             </p>
         </Container>
     )
